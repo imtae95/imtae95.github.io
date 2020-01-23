fc6669c7f8a1ce26aa6ed1d813ba5a45b430cb99
Imtae <95imtae@gmail.com>
2020-01-23 21:20:32 +0900

    init: create-react-app

---
+++ precache-manifest.ce20cff45578ca4ff12a657b996454a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d3c51b0b87f9602033207448b494a08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4723a0ddf551fa8fa808",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "53ce5d0dc126a28c432b",
+    "url": "/static/js/2.ce5eda19.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ce5eda19.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4723a0ddf551fa8fa808",
+    "url": "/static/js/main.dfede3cd.chunk.js"
+  },
+  {
+    "revision": "d723f2e3251c973fb873",
+    "url": "/static/js/runtime-main.77d3be62.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- script.js
+++
@@ -1,5 +0,0 @@
-const bodyEl = document.querySelector(`body`);
-
-bodyEl.addEventListener(`click`, () => {
-  alert(`Hello world!`);
-});
